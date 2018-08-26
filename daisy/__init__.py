7b91ae38425b9e8bb996021fc442e7e293a77aca
Jan Funke <jfunke@iri.upc.edu>
2018-08-26 12:16:36 -0400

    Expose daisy.processes.call as daisy.call

--- daisy/__init__.py
+++ daisy/__init__.py
@@ -3,4 +3,5 @@ from .blocks import create_dependency_graph
 from .coordinate import Coordinate
 from .dask_scheduler import run_blockwise
 from .datasets import open_ds, prepare_ds
+from .processes import call
 from .roi import Roi
