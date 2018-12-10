380a0b6903b3476726f63821992a7c1c95b16dda
Tri Nguyen <tri_nguyen@hms.harvard.edu>
2018-12-10 16:23:49 -0500

    Add documentation for daisy.DependencyGraph

--- daisy/dependency_graph.py
+++ daisy/dependency_graph.py
@@ -13,12 +13,19 @@ from .coordinate import Coordinate
 
 logger = logging.getLogger(__name__)
 
-
 class DependencyGraph():
+    '''This class constructs a block-wise dependency graph of a given
+    ``Task`` and its dependencies.It provides an interface for the
+    scheduler to query for blocks ready to be computed, and also a
+    mechanism to retry blocks that have failed.
+
+    User can make a subgraph of certain ROIs of the full graph through
+    ``get_subgraph``.
+    '''
 
     global_config = None
 
-    root_task_id = None
+    leaf_task_id = None
     tasks = set()
     task_map = {}
     prepared_tasks = set()
@@ -42,14 +49,20 @@ class DependencyGraph():
     def __init__(self, global_config):
         self.global_config = global_config
 
-
     def add(self, task):
+        '''Add a ``Task`` to the graph.
+
+        Args:
+            task(``Task``):
+                Task to be added to the graph. Its dependencies are
+                automatically and recursively added to the graph.
+        '''
         task.init_from_config(self.global_config)
 
-        if self.root_task_id == None:
-            self.root_task_id = task.task_id
+        if self.leaf_task_id is None:
+            self.leaf_task_id = task.task_id
         else:
-            logger.error("Daisy can only have 1 final task currently.")
+            logger.error("Daisy can only have one leaf task currently.")
             raise
 
         self.tasks.add(task)
@@ -57,6 +70,7 @@ class DependencyGraph():
         self.add_task_dependency(task)
 
     def add_task_dependency(self, task):
+        '''Recursively add dependencies of a task to the graph.'''
         for dependency_task in task.requires():
             dependency_task.init_from_config(self.global_config)
             self.tasks.add(dependency_task)
@@ -67,18 +81,19 @@ class DependencyGraph():
             self.add_task_dependency(dependency_task)
 
     def init(self):
-        self.__recursively_prepare(self.root_task_id)
-        self.__recursively_create_dependency_graph(self.root_task_id)
-
-        # print(self.blocks)
-        # print(self.dependencies)
+        '''Called by the ``scheduler`` after all tasks have been added.
+        Call the prepare() of each task, and create the entire
+        block-wise graph.'''
+        self.__recursively_prepare(self.leaf_task_id)
+        self.__recursively_create_dependency_graph(self.leaf_task_id)
 
     def __recursively_create_dependency_graph(self, task_id):
-        ''' Create dependency graph for its dependencies first before own '''
-
+        '''Create dependency graph for its dependencies first before
+        its own'''
         if task_id in self.created_tasks:
             return
-        self.created_tasks.add(task_id)
+        else:            
+            self.created_tasks.add(task_id)
 
         for dependency_task in self.task_dependency[task_id]:
             self.__recursively_create_dependency_graph(dependency_task)
@@ -96,11 +111,13 @@ class DependencyGraph():
         self.task_total_block_count[task_id] = len(blocks)
         self.task_done_count[task_id] = 0
 
-        # some sanity check
+        # some sanity checks
         assert(task.max_retries >= 0)
 
-        # post process this graph with our own scheduler
+        # add tasks to block-wise graph, while accounting for intra-task
+        # and inter-task dependencies
         for block, block_dependencies in blocks:
+
             block_id = (task_id, block.block_id)
             self.blocks[block_id] = block
 
@@ -111,7 +128,6 @@ class DependencyGraph():
                 roi = block.read_roi
                 for dependent_task in self.task_dependency[task_id]:
                     block_ids = self._get_subgraph_blocks(dependent_task, roi)
-                    # print(blocks)
                     dependencies.extend([(dependent_task, block_id) for block_id in block_ids])
 
             for dep_id in dependencies:
@@ -119,7 +135,8 @@ class DependencyGraph():
                 self.dependencies[block_id].add(dep_id)
 
             if len(dependencies) == 0:
-                # if no dependencies, add to ready queue immediately
+                # if this block has no dependencies, add it to the ready
+                # queue immediately
                 self.ready_queue.append(block_id)
 
     def __recursively_prepare(self, task):
@@ -134,11 +151,17 @@ class DependencyGraph():
         self.task_map[task].prepare()
 
     def next(self):
-        ''' Get the next available block.
-            Blocks and waits for outstanding blocks if there is none ready.
+        '''Called by the ``scheduler`` to get the next available block.
+        Current implementation blocks and waits for outstanding (issued)
+        blocks if there is none ready. After an outstanding block
+        finished, a new block may become ready and will be returned.
 
-            Returns tuple (task_name, block) if available.
-            Returns `None` if there will be no more blocks to run.
+        Return:
+            Tuple (task_name, block) if available, or ``None``.
+            Returning ``None`` does not necessarily mean that there is
+            no more blocks to be run (though it is the case currently).
+            The scheduler should call empty() to really make sure that
+            there is no more blocks to run.
         '''
 
         with self.ready_queue_cv:
@@ -148,45 +171,61 @@ class DependencyGraph():
             if self.empty():
                 return None
 
-            # block_id = self.ready_queue.popleft()
+            # TODO: add option for graph execution method
+            # pop() gives DFS-style execution (new blocks first)
+            # popleft() would give BFS
             block_id = self.ready_queue.pop()
             self.processing_blocks.add(block_id)
             return (block_id[0], self.blocks[block_id])
 
     def get_tasks(self):
+        '''Get all tasks in the graph.'''
         return self.tasks
 
     def empty(self):
+        '''Return ``True`` if there is no more blocks to be executed,
+        either because all blocks have been completed, or because there
+        are failed blocks that prevent other blocks from running.'''
         return ((len(self.ready_queue) == 0)
                  and (len(self.processing_blocks) == 0))
 
     def size(self):
+        '''Return the size of the block-wise graph.'''
         return len(self.blocks)
 
     def ready_size(self):
+        '''Return the number of blocks ready to be run.'''
         return len(self.ready_queue)
 
     def get_orphans(self):
+        '''Return the number of blocks cannot be issued due to failed
+        dependencies.'''
         return self.orphaned_blocks
 
     def get_failed_blocks(self):
+        '''Return blocks that have failed and won't be retried.'''
         return self.failed_blocks
 
     def get_block(self, block_id):
+        '''Return a specific block.'''
         return self.blocks[block_id]
 
     def cancel_and_reschedule(self, block_id):
-        # assert(block_id in self.processing_blocks)
+        '''Used to notify that a block has failed. The block will either
+        be rescheduled if within the number of retries, or be marked
+        as failed.'''
         if block_id not in self.processing_blocks:
-            logger.error("Block {} is canceled but was not found"
-                         .format(block_id))
+            logger.error(
+                "Block %d is canceled but was not found", block_id)
             raise
 
         self.retry_count[block_id] = self.retry_count[block_id] + 1
 
         with self.ready_queue_cv:
+
             self.processing_blocks.remove(block_id)
             task_name = block_id[0]
+
             if self.retry_count[block_id] > self.task_map[task_name].max_retries:
 
                 self.failed_blocks.add(block_id)
@@ -203,12 +242,14 @@ class DependencyGraph():
                 # simply leave it canceled at this point
 
             else:
+
                 self.ready_queue.appendleft(block_id)
                 logger.info("Block {} will be rescheduled.".format(block_id))
 
             self.ready_queue_cv.notify()  # in either case, unblock next()
 
     def recursively_check_orphans(self, block_id):
+        '''Check and mark children of the given block as orphans.'''
         for orphan_id in self.dependents[block_id]:
 
             if (orphan_id in self.orphaned_blocks
@@ -219,74 +260,62 @@ class DependencyGraph():
             self.recursively_check_orphans(orphan_id)
 
     def remove_and_update(self, block_id):
-        '''Removing a finished block and update ready queue'''
+        '''Removing a finished block and update ready queue.'''
         with self.ready_queue_cv:
+
             self.task_done_count[block_id[0]] += 1
             self.processing_blocks.remove(block_id)
+
             dependents = self.dependents[block_id]
             for dep in dependents:
                 self.dependencies[dep].remove(block_id)
                 if len(self.dependencies[dep]) == 0:
                     # ready to run
                     self.ready_queue.append(dep)
+
+            # Unblock next() regardless. If we only unblock for new
+            # elements in ready_queue, the program might lock up if
+            # this block is the last
             self.ready_queue_cv.notify()
 
     def get_subgraph(self, roi):
-
+        '''Create a subgraph given a ROI, assuming this ROI is that
+        of the leaf task.'''
         subgraph = copy.deepcopy(self)
         subgraph.__create_subgraph(roi)
         return subgraph
 
     def __create_subgraph(self, roi):
-        ''' Modify existing graph so that when executed, only the minimum
-            number of blocks will be computed to cover the given ROI.
+        '''Modify existing graph so that only the minimum number of
+        blocks will be computed to cover the given ROI. This is achieved
+        by simply recomputing the ready_queue as computed for the full
+        graph.'''
+        self.ready_queue.clear()
 
-            This is achieved simply by removing relevant blocks from
-            the ready_queue as computed for the full graph
-        '''
-
-        # check if everything is initiated correctly
-        # get primary task, and create its new roi
-        # print(self.ready_queue)
-
+        # get blocks of the leaf task that writes to the given ROI
         to_check = collections.deque()
         to_check.extend(
-            [(self.root_task_id, block)
-                for block in self._get_subgraph_blocks(self.root_task_id, roi)
+            [(self.leaf_task_id, block)
+                for block in self._get_subgraph_blocks(self.leaf_task_id, roi)
             ])
             
-        # relevant_blocks = set()
-        self.ready_queue.clear()
         processed = set()
-
         while len(to_check) > 0:
-            # print(to_check)
+
             block = to_check.popleft()
+
             if block in processed:
                 continue
-            processed.add(block)
+            else:
+                processed.add(block)
 
             if len(self.dependencies[block]) == 0:
-                # relevant_blocks.add(block)
                 self.ready_queue.append(block)
             else:
                 to_check.extend(self.dependencies[block])
 
-        # print(self.ready_queue)
-
-
-        # self.ready_queue.clear()
-        # self.ready_queue.
-
-
-        # get the list of blocks that contribute to this roi
-        # if it's in the ready block list, add it to a separate list
-        # check its dependency, adding them to a to-be-checked list
-        # check one task at a time to minimize redundancy
-        # particularly at a join, wait for the other path to finish first
-
     def _get_subgraph_blocks(self, task_id, roi):
-        ''' Returns a blocks of this task that write to given ROI '''
+        '''Return blocks of this task that write to given ROI.'''
         task = self.task_map[task_id]
         return get_subgraph_blocks(
             roi,
@@ -296,4 +325,6 @@ class DependencyGraph():
             task.fit)
 
     def is_task_done(self, task_id):
-        return self.task_done_count[task_id] == self.task_total_block_count[task_id]
+        '''Return ``True`` if all blocks of a task have completed.'''
+        return (self.task_done_count[task_id]
+                == self.task_total_block_count[task_id])
