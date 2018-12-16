178fabe42484842edc5ce43a4ca1793788034b41
Jan Funke <funkej@janelia.hhmi.org>
2018-12-16 13:58:04 -0500

    Expose daisy.Graph

--- daisy/__init__.py
+++ daisy/__init__.py
@@ -4,6 +4,7 @@ from .blocks import create_dependency_graph
 from .coordinate import Coordinate
 from .dask_scheduler import run_blockwise
 from .datasets import open_ds, prepare_ds
+from .graph import Graph
 from .processes import call
 from .roi import Roi
 from . import persistence
