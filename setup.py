867f6092d53f52d40f66af6bb03042501731e623
Jan Funke <funkej@janelia.hhmi.org>
2018-12-17 08:53:31 -0500

    Update and simplify setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,12 @@
 from setuptools import setup
-import subprocess
-try:
-    import base_string as string_types
-except ImportError:
-    string_types = str
-
-extras_require = {
-}
-
-
-dep_set = set()
-for value in extras_require.values():
-    if isinstance(value, string_types):
-        dep_set.add(value)
-    else:
-        dep_set.update(value)
-
-extras_require['full'] = list(dep_set)
 
 setup(
         name='daisy',
         version='0.1',
         description='Block-wise task dependencies for luigi.',
-        url='https://github.com/funkey/daisy',
+        url='https://github.com/funkelab/daisy',
         author='Jan Funke',
-        author_email='jfunke@iri.upc.edu',
+        author_email='funkej@janelia.hhmi.org',
         license='MIT',
         packages=[
             'daisy',
@@ -37,6 +19,5 @@ setup(
             "networkx",
             "numpy",
             "pymongo"
-        ],
-        extras_require=extras_require,
+        ]
 )
