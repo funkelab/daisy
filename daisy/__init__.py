96e24092b88ec4532401b415a1dfe2ccdaef8f1c
Arlo Sheridan <sheridana@janelia.hhmi.org>
2019-01-16 16:23:35 -0500

    Expose daisy.Context in public API

--- daisy/__init__.py
+++ daisy/__init__.py
@@ -3,6 +3,7 @@ from . import persistence # noqa
 from .array import Array # noqa
 from .blocks import create_dependency_graph # noqa
 from .client_scheduler import ClientScheduler # noqa
+from .context import Context # noqa
 from .coordinate import Coordinate # noqa
 from .datasets import open_ds, prepare_ds # noqa
 from .dependency_graph import DependencyGraph # noqa
