2eda04ff8e87081fcbfa6c3592f062c4edf18ea7
Tri M. Nguyen <tri_nguyen@hms.harvard.edu>
2019-01-17 11:46:10 -0500

    Implement better Task Parameter
    There are now 3 ways for users to set `Parameter` for a `Task`:
    1. Through an argument to `Task` (e.g. t = DaisyTask(test_param=val))
    2. Through a dictionary config file
    3. As a default value of the Parameter.
    
    The order of precedence is as above.
    
    With inherited Tasks, same-name, duplicated Parameter will
    override any inherited Parameters from the base Tasks, including
    any default values. This allows inherited Tasks to unset
    default values which then forces the user to give new values
    through either #1 or #2 option.

--- daisy/task.py
+++ daisy/task.py
@@ -1,4 +1,4 @@
-from .parameter import Parameter
+from .parameter import Parameter, UNDEFINED_DAISY_PARAMETER
 
 
 class Task():
@@ -33,12 +33,24 @@ class Task():
             return a list of ``Task``s that this one depends on.
     '''
 
-    # Unique identifier for this task. By default it is set to the
-    # (sub)class name, but can be overridden in the constructor
-    task_id = Parameter()
-
     log_to_files = Parameter(default=False)
     log_to_stdout = Parameter(default=True)
+
+    def inheritParameters(self, current_class):
+        '''Recursively query and inherit `Parameter`s from base `Task`s.
+        Parameters are copied to self.__daisy_params__ to be processed
+        downstream.
+
+        If duplicated, `Parameter`s of derived `Task`s will override ones
+        from base `Task`s, even if it clears any default. This lets
+        inherited `Task` to unset defaults and force user to input new
+        values.'''
+        for b in current_class.__bases__:
+            self.inheritParameters(b)
+
+        for param in current_class.__dict__:
+            if isinstance(current_class.__dict__[param], Parameter):
+                self.__daisy_params__[param] = current_class.__dict__[param]
 
     def __init__(self, task_id=None, **kwargs):
         '''Constructor for ``Task``. Should not be overridden by
@@ -65,11 +77,18 @@ class Task():
             # default task ID is the class name
             self.task_id = type(self).__name__
 
+        self.__daisy_params__ = {}
+        self.inheritParameters(self.__class__)
+
+        # applying user input parameters
         for key in kwargs:
-            # TODO: we might want to check whether the class actually
-            # specified these Parameters and not blindly setting them
-            value = kwargs[key]
-            setattr(self, key, value)
+            if key in self.__daisy_params__:
+                self.__daisy_params__[key].set(kwargs[key])
+            else:
+                raise RuntimeError(
+                        "Key %s not found in "
+                        "Parameter list for Task %s" %
+                        (key, self.task_id))
 
     def init_from_config(self, global_config):
         '''Daisy calls this function internally if a global config was
@@ -85,12 +104,33 @@ class Task():
             return
 
         config = global_config[self.task_id]
+
         for key in config:
-            if hasattr(self, key) and getattr(self, key) is not None:
-                # do not override config from __init__()
-                continue
-            value = config[key]
-            setattr(self, key, value)
+
+            if key in self.__daisy_params__:
+
+                if not self.__daisy_params__[key].user_spec:
+                    self.__daisy_params__[key].val = config[key]
+                else:
+                    # do not override user input from __init__()
+                    print("Skip file setting %s to %s, user value %s" %
+                          (key, config[key], self.__daisy_params__[key].val))
+                    continue
+
+            else:
+                raise RuntimeError(
+                        "Key %s is not in the Parameter list for Task %s" %
+                        (key, self.task_id))
+
+        # finalize parameters
+        for param in self.__daisy_params__:
+            val = self.__daisy_params__[param].val
+            if val is UNDEFINED_DAISY_PARAMETER:
+                raise RuntimeError(
+                    "Parameter %s of %s is unassigned! You can probably fix "
+                    "this by passing in `default` such as `None`" %
+                    (param, self.task_id))
+            setattr(self, param, val)
 
     def prepare(self):
         '''Subclasses override this to perform setup actions'''
