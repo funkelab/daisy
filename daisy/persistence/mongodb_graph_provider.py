280bea3db033829ea40844957ca5d30fe5fbc5e5
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2019-03-05 12:17:47 -0500

    Create abstract write function for mongo subgraphs
    Allows code sharing across writing nodes and edges,
    and explicitly checks that the flag requirements are true
    before writing any nodes/edges.

--- daisy/persistence/mongodb_graph_provider.py
+++ daisy/persistence/mongodb_graph_provider.py
@@ -39,7 +39,8 @@ class MongoDbGraphProvider(SharedGraphProvider):
 
         directed (``bool``):
 
-            True if the graph is directed, false otherwise
+            True if the graph is directed, false otherwise. If None, attempts
+            to read value from existing database. If fails, defaults to false.
 
         nodes_collection (``string``):
         edges_collection (``string``):
@@ -477,36 +478,10 @@ class MongoDbSharedSubGraph(SharedSubGraph):
             return
 
         try:
-            if fail_if_exists:
-                self.edges_collection.insert_many(edges)
-            elif fail_if_not_exists:
-                result = self.edges_collection.bulk_write([
-                    ReplaceOne(
-                        {
-                            u_name: int(np.int64(edge[u_name])),
-                            v_name: int(np.int64(edge[v_name]))
-                        },
-                        edge,
-                        upsert=False
-                    )
-                    for edge in edges
-                ])
-                raise WriteError(
-                        details="{} edges did not exist and were not written"
-                        .format(len(edges) - result.matched_count))
-            else:
-                self.edges_collection.bulk_write([
-                    ReplaceOne(
-                        {
-                            u_name: int(np.int64(edge[u_name])),
-                            v_name: int(np.int64(edge[v_name]))
-                        },
-                        edge,
-                        upsert=True
-                    )
-                    for edge in edges
-                ])
-
+            self.__write(self.edges_collection, [u_name, v_name], edges,
+                         fail_if_exists=fail_if_exists,
+                         fail_if_not_exists=fail_if_not_exists,
+                         delete=delete)
         except BulkWriteError as e:
 
             logger.error(e.details)
@@ -555,36 +530,10 @@ class MongoDbSharedSubGraph(SharedSubGraph):
             return
 
         try:
-            if fail_if_exists:
-                self.nodes_collection.insert_many(nodes)
-            elif fail_if_not_exists:
-                result = self.nodes_collection.bulk_write([
-                    ReplaceOne(
-                        {
-                            'id': int(np.int64(node['id'])),
-                        },
-                        node,
-                        upsert=False
-                    )
-                    for node in nodes
-                ])
-                if result.matched_count != len(nodes):
-                    raise WriteError(
-                            details="{} nodes didn't exist and weren't written"
-                            .format(len(nodes) - result.matched_count))
-
-            else:
-                self.nodes_collection.bulk_write([
-                    ReplaceOne(
-                        {
-                            'id': int(np.int64(node['id'])),
-                        },
-                        node,
-                        upsert=True
-                    )
-                    for node in nodes
-                ])
-
+            self.__write(self.nodes_collection, ['id'], nodes,
+                         fail_if_exists=fail_if_exists,
+                         fail_if_not_exists=fail_if_not_exists,
+                         delete=delete)
         except BulkWriteError as e:
 
             logger.error(e.details)
@@ -610,6 +559,69 @@ class MongoDbSharedSubGraph(SharedSubGraph):
             subgraphs.append(g)
 
         return subgraphs
+
+    def __write(self, collection, match_fields, docs,
+                fail_if_exists=False, fail_if_not_exists=False, delete=False):
+        '''Writes documents to provided mongo collection, checking for restricitons.
+
+        Args:
+
+            collection (``pymongo.collection``):
+
+                The collection to write the documents into.
+
+            match_fields (``list`` of ``string``):
+
+                The set of fields to match to be considered the same document.
+
+            docs (``dict`` or ``bson``):
+
+                The documents to insert into the collection
+
+            fail_if_exists, fail_if_not_exists, delete (``bool``):
+
+                see write_nodes or write_edges for explanations of these flags
+            '''
+        assert not delete, "Delete not implemented"
+        match_docs = []
+        for doc in docs:
+            match_doc = {}
+            for field in match_fields:
+                match_doc[field] = doc[field]
+            match_docs.append(match_doc)
+
+        if fail_if_exists:
+            self.__write_fail_if_exists(collection, match_docs, docs)
+        elif fail_if_not_exists:
+            self.__write_fail_if_not_exists(collection, match_docs, docs)
+        else:
+            self.__write_no_flags(collection, match_docs, docs)
+
+    def __write_no_flags(self, collection, old_docs, new_docs):
+        bulk_query = [ReplaceOne(old, new, upsert=True)
+                      for old, new in zip(old_docs, new_docs)]
+        collection.bulk_write(bulk_query)
+
+    def __write_fail_if_exists(self, collection, old_docs, new_docs):
+        for old in old_docs:
+            if collection.find(old):
+                raise WriteError(
+                        "Found existing doc %s and fail_if_exists set to True."
+                        " Aborting write for all docs." % old)
+        collection.insert_many(new_docs)
+
+    def __write_fail_if_not_exists(self, collection, old_docs, new_docs):
+        for old in old_docs:
+            if not collection.find(old):
+                raise WriteError(
+                        "Did not find existing doc %s and fail_if_not_exists "
+                        "set to True. Aborting write for all docs." % old)
+        bulk_query = [ReplaceOne(old, new, upsert=False)
+                      for old, new in zip(old_docs, new_docs)]
+        result = collection.bulk_write(bulk_query)
+        assert len(new_docs) == result.matched_count,\
+            ("Supposed to replace %s docs, but only replaced %s"
+                % (len(new_docs), result.matched_count))
 
     def __contains(self, roi, node):
         '''Determines if the given node is inside the given roi'''
