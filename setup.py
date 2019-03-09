63d00a6322b0458c9773bfd70505f763f89ee538
Tri M. Nguyen <tri_nguyen@hms.harvard.edu>
2019-03-09 17:13:34 -0500

    Restrict dependency `tornado` to be 5.x
    Tornado 6.x is not tested (yet) and I have seen it to not work in my
    configuration.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
         ],
         install_requires=[
             "numpy",
-            "tornado",
+            "tornado>=5,<6",
             "networkx",
             "pymongo"
         ]
