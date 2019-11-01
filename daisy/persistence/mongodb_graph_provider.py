4663299ce53ac771d417140bdbc29149eed7e17c
pattonw <pattonw@hhmi.org>
2019-11-01 16:46:06 -0400

    fix whitespace

--- daisy/persistence/mongodb_graph_provider.py
+++ daisy/persistence/mongodb_graph_provider.py
@@ -332,8 +332,8 @@ class MongoDbGraphProvider(SharedGraphProvider):
                         ]
                     }
                 else:
-                endpoint_query = {self.endpoint_names[0]:
-                                  {'$in': node_ids[i_b:i_e]}}
+                    endpoint_query = {self.endpoint_names[0]:
+                                      {'$in': node_ids[i_b:i_e]}}
                 if attr_filter:
                     query = {'$and': filters + [endpoint_query]}
                 else:
@@ -362,9 +362,9 @@ class MongoDbGraphProvider(SharedGraphProvider):
                 nodes += additional_nodes
 
             else:
-        for edge in edges:
-            edge[u] = np.uint64(edge[u])
-            edge[v] = np.uint64(edge[v])
+                for edge in edges:
+                    edge[u] = np.uint64(edge[u])
+                    edge[v] = np.uint64(edge[v])
 
         finally:
 
