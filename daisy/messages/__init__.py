6eedf8a7c1f078cc84ed6479ae5ce2eddbd33d84
Jan Funke <funkej@janelia.hhmi.org>
2020-09-30 19:29:24 +0200

    Add SendBlock and TerminateClient messages

--- daisy/messages/__init__.py
+++ daisy/messages/__init__.py
@@ -2,3 +2,5 @@ from .message import Message, ExceptionMessage
 from .acquire_block import AcquireBlock
 from .release_block import ReleaseBlock
 from .client_exception import ClientException
+from .send_block import SendBlock
+from .terminate_client import TerminateClient
---
+++ daisy/messages/terminate_client.py
@@ -0,0 +1,5 @@
+from .message import Message
+
+
+class TerminateClient(Message):
+    pass
