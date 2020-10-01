1d1a903b8ece8a5b2a61a127fdf360740475ad66
Jan Funke <funkej@janelia.hhmi.org>
2020-10-02 00:30:52 +0200

    Improve separation of TCP and application layers
    With this commit, daisy.tcp is self-contained. Only TCPMessages are sent
    between TCP{Client,Server}, no knowledge of application messages is
    required. Application messages should inherit from TCPMessages.
    
    Furthermore, TCPClient.disconnect() does now ensure a graceful
    disconnect and closing of the stream on both sides. Only unexpected
    StreamClosedErrors (due to network problems or server crashes) are
    propagated to the application layer, reducing guesswork on the
    application side about the cause of a closed stream.

--- daisy/tcp/tcp_client.py
+++ daisy/tcp/tcp_client.py
@@ -1,77 +1,103 @@
+from .exceptions import NotConnected
+from .io_looper import IOLooper
+from .tcp_stream import TCPStream
+from .internal_messages import (
+    AckClientDisconnect,
+    NotifyClientDisconnect)
 import logging
 import queue
-import sys
 import time
 import tornado.tcpclient
 
-from .io_looper import IOLooper
-from .tcp_stream import TCPStream
-from daisy.messages import ExceptionMessage
-
 logger = logging.getLogger(__name__)
-
-# TODO: Do we want to close the stream or ioloop or anything?
-'''
-
-    def __del__(self):
-        Stop spawn ioloop when client is done
-        try:
-            self.ioloop.add_callback(self.ioloop.stop)
-        except Exception:
-            # self.ioloop is None
-            pass
-
-'''
 
 
 class TCPClient(IOLooper):
     '''A TCP client to handle client-server communication through
-    :class:`Message` objects.
+    :class:`TCPMessage` objects.
 
     Args:
 
         host (string):
         port (int):
+
             The hostname and port of the :class:`TCPServer` to connect to.
     '''
+
     def __init__(self, host, port):
 
         super().__init__()
 
-        logger.debug("Creating new client...")
+        logger.debug("Creating new TCP client...")
 
         self.host = host
         self.port = port
         self.client = tornado.tcpclient.TCPClient()
         self.stream = None
         self.message_queue = queue.Queue()
-        self.error_state = False
+        self.exception_queue = queue.Queue()
 
-        logger.debug("Connecting to server...")
+        self.connect()
+
+    def __del__(self):
+
+        if self.connected():
+            self.disconnect()
+
+    def connect(self):
+        '''Connect to the server and start the message receive event loop.'''
+
+        logger.debug("Connecting to server at %s:%d...", self.host, self.port)
 
         self.ioloop.add_callback(self._connect)
-
-        logger.debug("Waiting for connection to be established...")
-        while not self.stream:
-            if self.error_state:
-                logger.error("Could not connect to server")
-                sys.exit(1)
+        while not self.connected():
+            self._check_for_errors()
             time.sleep(.1)
+
         logger.debug("...connected")
 
         self.ioloop.add_callback(self._receive)
 
-    def send_message(self, message):
-        '''Send a message to the server asynchronously. Same as::
+    def disconnect(self):
+        '''Gracefully close the connection to the server and stop the
+        IOLoop.'''
 
-            self.stream.send_message(message)
+        if not self.connected():
+            logger.warn("Called disconnect() on disconnected client")
+            return
+
+        logger.debug("Notifying server of disconnect...")
+        self.stream.send_message(NotifyClientDisconnect())
+
+        while self.connected():
+            time.sleep(.1)
+
+        # TCPServer is no longer listening to this client, now it is safe to
+        # close the stream and stop the IOLoop
+        logger.debug("Stopping IOLoop")
+        self.ioloop.add_callback(self.ioloop.stop)
+
+        logger.debug("Disconnected")
+
+    def connected(self):
+        '''Check whether this client has a connection to the server.'''
+        return self.stream is not None
+
+    def send_message(self, message):
+        '''Send a message to the server.
 
         Args:
 
-            message (:class:`daisy.Message`):
+            message (:class:`TCPMessage`):
 
-                Message to send over the connection.
+                Message to send over to the server.
         '''
+
+        self._check_for_errors()
+
+        if not self.connected():
+            raise NotConnected()
+
         self.stream.send_message(message)
 
     def get_message(self, timeout=None):
@@ -79,51 +105,77 @@ class TCPClient(IOLooper):
 
         Args:
 
-            timeout (int, optional):
+            timeout (``float``, optional):
 
                 If set, wait up to `timeout` seconds for a message to arrive.
                 If no message is available after the timeout, returns ``None``.
                 If not set, wait until a message arrived.
         '''
+
+        self._check_for_errors()
+
+        if not self.connected():
+            raise NotConnected()
+
         try:
 
-            message = self.message_queue.get(block=True, timeout=timeout)
-            if isinstance(message, ExceptionMessage):
-                raise message.exception
-            return message
+            return self.message_queue.get(block=True, timeout=timeout)
 
         except queue.Empty:
 
             return None
 
-    async def _connect(self):
+    def _check_for_errors(self):
 
-        logger.debug("Connecting to %s:%d...", self.host, self.port)
+        try:
+            exception = self.exception_queue.get(block=False)
+            raise exception
+        except queue.Empty:
+            return
+
+    async def _connect(self):
+        '''Async method to connect to the TCPServer.'''
+
         try:
             stream = await self.client.connect(self.host, self.port)
-            # TODO: Add a timeout? previously had 60 second timeout and 10
-            # retries
-        except Exception:
-            self.error_state = True
-            raise
-        self.stream = TCPStream(stream)
+        except Exception as e:
+            self.exception_queue.put(e)
+            return
 
-        logger.debug("...connected")
+        self.stream = TCPStream(stream)
 
     async def _receive(self):
         '''Loop that receives messages from the server.'''
 
         logger.debug("Entering receive loop")
 
-        while True:
+        while self.connected():
+
             try:
 
+                # raises StreamClosedError
                 message = await self.stream._get_message()
-                if message is None:
-                    break
-                self.message_queue.put(message)
+
+                if isinstance(message, AckClientDisconnect):
+
+                    # server acknowledged disconnect, close connection on
+                    # our side and break out of event loop
+                    try:
+                        self.stream.close()
+                    finally:
+                        self.stream = None
+                        return
+
+                else:
+
+                    self.message_queue.put(message)
 
             except Exception as e:
 
-                logger.error("TCPClient %s got Exception %s", self, e)
-                break
+                try:
+                    self.exception_queue.put(e)
+                    self.stream.close()
+                finally:
+                    # mark client as disconnected
+                    self.stream = None
+                    return
---
+++ daisy/tcp/tcp_message.py
@@ -0,0 +1,19 @@
+class TCPMessage:
+    '''A message, to be sent between :class:`TCPServer` and :class:`TCPClient`.
+
+    Args:
+
+        payload (pickleable):
+            The payload.
+
+    Attributes:
+
+        stream (:class:`TCPStream`):
+            The stream the message was received from. Will be set by
+            :class:`TCPStream` and is ``None`` for messages that have not been
+            sent.
+    '''
+    def __init__(self, payload=None):
+
+        self.payload = payload
+        self.stream = None
