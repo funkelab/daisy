3d6220479aff4ef8071b23ab8220485136b51e95
pattonw <pattonw@hhmi.org>
2020-10-02 17:22:52 -0400

    remove z_index from block

--- daisy/block.py
+++ daisy/block.py
@@ -6,12 +6,15 @@ from funlib.math import cantor_number
 import copy
 from enum import Enum
 
+
 class BlockStatus(Enum):
+    CREATED = 0
     SUCCESS = 1
+    FAILED = 2
 
 
 class Block(Freezable):
-    '''Describes a block to process with attributes:
+    """Describes a block to process with attributes:
 
     Attributes:
 
@@ -48,19 +51,18 @@ class Block(Freezable):
             The ID to assign to this block. The ID is normally computed from
             the write ROI and the total ROI, such that each block has a unique
             ID.
-    '''
+    """
 
-    def __init__(self, total_roi, read_roi, write_roi, block_id=None):
+    def __init__(self, total_roi, read_roi, write_roi, task_id, block_id=None):
 
         self.read_roi = read_roi
         self.write_roi = write_roi
 
+        self.task_id = task_id
         if block_id is None:
-            self.block_id, self.z_order_id = self.__compute_block_id(
-                    total_roi, write_roi)
-        else:
-            self.block_id = block_id
-            self.z_order_id = block_id  # for compatibility
+            block_id = self.__compute_block_id(total_roi, write_roi)
+        self.block_id = (task_id, block_id)
+        self.status = BlockStatus.CREATED
         self.freeze()
 
     def copy(self):
@@ -73,29 +75,12 @@ class Block(Freezable):
         # block_id will be the cantor number for this block index
         block_id = int(cantor_number(block_index))
 
-        # calculating Z-order index
-        # this index is inexact and is shape agnostic to promote maximum
-        # parallelism across tasks
-        block_index_z = (write_roi.get_offset() /
-                         Coordinate([2048 for _ in range(total_roi.dims())]))
-        bit32_constant = 1 << 31
-        indices = [int(block_index_z[i]) for i in range(total_roi.dims())]
-
-        n = 0
-        z_order_id = 0
-        while n < 32:
-            for i in range(total_roi.dims()):
-                z_order_id = z_order_id >> 1
-                if indices[i] & 1:
-                    z_order_id += bit32_constant
-                indices[i] = indices[i] >> 1
-                n += 1
-
-        return block_id, z_order_id
+        return block_id
 
     def __repr__(self):
 
-        return "id: %d (read_roi: %s, write_roi %s)" % (
+        return "id: %s (read_roi: %s, write_roi %s)" % (
             self.block_id,
             self.read_roi,
-            self.write_roi)
+            self.write_roi,
+        )
