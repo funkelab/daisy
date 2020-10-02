06adbd92693dc459d9615c405d6fdeece932ae54
pattonw <pattonw@hhmi.org>
2020-10-02 15:38:31 -0400

    remove create_dependency_graph form blocks

--- daisy/__init__.py
+++ daisy/__init__.py
@@ -2,7 +2,6 @@ from __future__ import absolute_import
 from . import persistence # noqa
 from .array import Array # noqa
 from .block import Block, BlockStatus # noqa
-from .blocks import create_dependency_graph # noqa
 from .blocks import expand_roi_to_grid # noqa
 from .blocks import expand_write_roi_to_grid # noqa
 from .client import Client # noqa
