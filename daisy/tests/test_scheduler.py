35a859a903a2bf52162d654a42a0a45ac01c2704
pattonw <pattonw@hhmi.org>
2020-10-02 17:23:17 -0400

    update scheduler tests

--- daisy/tests/test_scheduler.py
+++ daisy/tests/test_scheduler.py
@@ -1,128 +1,125 @@
 from daisy import Scheduler
 from daisy import Task
 from daisy import Block, BlockStatus
-from daisy import Roi, Parameter
+from daisy import Roi
 
 import pytest
+
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 @pytest.fixture
 def task_1d(tmpdir):
-    return Task1D(outdir=tmpdir)
+    # block ids:
+    # 1, 2
 
-
-class Task1D(Task):
-    outdir = Parameter()
-
-    def __init__(self, **kwargs):
-        super().__init__(
-            0,
-            total_roi=Roi((0,), (4,)),
-            read_roi=Roi((0,), (3,)),
-            write_roi=Roi((1,), (1,)),
-            process_function=None,
-            check_function=None,
-            **kwargs,
-        )
-
-    def requires(self):
-        return []
-
-    def prepare(self):
-        pass
+    # 2  ->  Level 1
+    # 1  ->  Level 2
+    return Task(
+        task_id="test_1d",
+        total_roi=Roi((0,), (4,)),
+        read_roi=Roi((0,), (3,)),
+        write_roi=Roi((1,), (1,)),
+        process_function=None,
+        check_function=None,
+    )
 
 
 @pytest.fixture
 def task_2d(tmpdir):
-    return Task2D(outdir=tmpdir)
+    # block ids:
+    # 4,  7,  11, 16
+    # 8,  12, 17, 23
+    # 13, 18, 24, 31
+    # 19, 25, 32, 40
 
-
-class Task2D(Task):
-    outdir = Parameter()
-
-    def __init__(self, **kwargs):
-        super().__init__(
-            0,
-            total_roi=Roi((0, 0), (6, 6)),
-            read_roi=Roi((0, 0), (3, 3)),
-            write_roi=Roi((1, 1), (1, 1)),
-            process_function=None,
-            check_function=None,
-            **kwargs,
-        )
-
-    def requires(self):
-        return []
-
-    def prepare(self):
-        pass
+    # if every block depends on its neighbors then there are 4 sets
+    # of 4 blocks that are safe to run in parallel in the order (1,1), (0,1), (1,0), (0,0)
+    # 12, 23, 25, 40  ->  Level 1
+    # 8,  17, 19, 32  ->  Level 2
+    # 7,  16, 18, 31  ->  Level 3
+    # 4,  11, 13, 24  ->  Level 4
+    return Task(
+        task_id="test_2d",
+        total_roi=Roi((0, 0), (6, 6)),
+        read_roi=Roi((0, 0), (3, 3)),
+        write_roi=Roi((1, 1), (1, 1)),
+        process_function=None,
+        check_function=None,
+    )
 
 
 def test_simple_acquire_block(task_1d):
-    scheduler = Scheduler([task_1d], {})
-    block = scheduler.acquire_block(task_1d)
-    # NOTE: block id is based on the cantor number of a blocks index if
-    # the total roi were tiled with cubes of size write_roi. Since we use
-    # the total roi and not the total write roi, the first block may not
-    # have index 0. In this case the total roi is (0, 4), but our first
-    # write block has roi (1, 1) and thus index 1, wheras roi (0, 1) would
-    # have index 0 but is never used.
-    expected_block_id = 2
+    scheduler = Scheduler([task_1d])
+    block = scheduler.acquire_block(task_1d.task_id)
+
     expected_block = Block(
-        Roi((1,), (3,)), Roi((1,), (3,)), Roi((2,), (1,)), block_id=expected_block_id
+        Roi((1,), (3,)), Roi((1,), (3,)), Roi((2,), (1,)), task_id="test_1d", block_id=2
     )
     assert block.read_roi == expected_block.read_roi
     assert block.write_roi == expected_block.write_roi
     assert block.block_id == expected_block.block_id
 
 
-def test_simple_release_block(task_2d):
-    scheduler = Scheduler([task_2d], {})
-    # expected block ordering:
-    # a, b, c, d
-    # e, f, g, h
-    # i, j, k, l
-    # m, n, o, p
-    # 4, 7, 11, 16
-    # 8, 12, 17, 23
-    # 13, 18, 24, 31
-    # 19, 25, 32, 40
+def test_simple_release_block(task_1d):
+    scheduler = Scheduler([task_1d])
+    block = scheduler.acquire_block(task_1d.task_id)
+    scheduler.release_block(block)
+    block = scheduler.acquire_block(task_1d.task_id)
 
-    # if every block depends on its neighbors then there are 4 sets
-    # of 4 blocks that are safe to run in parallel in the order (1,1), (0,1), (1,0), (0,0)
-    # f, h, n, p
-    # e, g, m, o
-    # b, d, j, l
-    # a, c, i, k
+    expected_block = Block(
+        Roi((0,), (3,)), Roi((0,), (3,)), Roi((1,), (1,)), task_id="test_1d", block_id=1
+    )
+    assert block.read_roi == expected_block.read_roi
+    assert block.write_roi == expected_block.write_roi
+    assert block.block_id == expected_block.block_id
 
-    # test first set: f, h, n, p
+
+def test_complete_task(task_2d):
+    scheduler = Scheduler([task_2d])
+
+    # test Level 1
     blocks = []
     for b in (12, 23, 25, 40):
-        block = scheduler.acquire_block(task_2d)
-        assert block.block_id == b
+        block = scheduler.acquire_block(task_2d.task_id)
+        block.status = BlockStatus.SUCCESS
+        assert block.block_id == ("test_2d", b)
         blocks.append(block)
 
     for block in blocks:
-        scheduler.release_block(task_2d, block, BlockStatus.SUCCESS)
+        scheduler.release_block(block)
 
-    # test second set: e, g, m, o
+    # test Level 2
     blocks = []
     for b in (8, 17, 19, 32):
-        block = scheduler.acquire_block(task_2d)
-        assert block.block_id == b
+        block = scheduler.acquire_block(task_2d.task_id)
+        block.status = BlockStatus.SUCCESS
+        assert block.block_id == ("test_2d", b)
         blocks.append(block)
 
     for block in blocks:
-        scheduler.release_block(task_2d, block, BlockStatus.SUCCESS)
+        scheduler.release_block(block)
 
-    # check first element of third set
-    expected_block_id = 7
-    block = scheduler.acquire_block(task_2d)
-    assert block.block_id == expected_block_id
-    scheduler.release_block(task_2d, block, BlockStatus.SUCCESS)
+    # test Level 3
+    blocks = []
+    for b in (7, 16, 18, 31):
+        block = scheduler.acquire_block(task_2d.task_id)
+        block.status = BlockStatus.SUCCESS
+        assert block.block_id == ("test_2d", b)
+        blocks.append(block)
 
-    # next should be the first element of fourth set
-    expected_block_id = 4
-    block = scheduler.acquire_block(task_2d)
-    assert block.block_id == expected_block_id
-    scheduler.release_block(task_2d, block, BlockStatus.SUCCESS)
+    for block in blocks:
+        scheduler.release_block(block)
+
+    # test Level 4
+    blocks = []
+    for b in (4, 11, 13, 24):
+        block = scheduler.acquire_block(task_2d.task_id)
+        block.status = BlockStatus.SUCCESS
+        assert block.block_id == ("test_2d", b)
+        blocks.append(block)
+
+    for block in blocks:
+        scheduler.release_block(block)
