d748802c1f12a1def66869b3ce0f3541fae6de99
Jan Funke <funkej@janelia.hhmi.org>
2020-10-05 16:54:53 +0200

    Use new Scheduler in Server test

--- daisy/tests/test_server.py
+++ daisy/tests/test_server.py
@@ -4,44 +4,6 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
-
-class DummyTaskState:
-
-    def __init__(self, p, r):
-        self.num_pending_blocks = p
-        self.num_ready_blocks = r
-
-
-class DummyScheduler:
-
-    def __init__(self, tasks, num_blocks):
-        self.tasks = tasks
-        self.num_blocks = num_blocks
-        print("DummyScheduler got some tasks!")
-
-    def get_ready_tasks(self):
-        return self.tasks
-
-    def acquire_block(self, task_id):
-
-        if self.num_blocks == 0:
-            return None, DummyTaskState(0, 0)
-
-        self.num_blocks -= 1
-
-        block = daisy.Block(
-            total_roi=self.tasks[0].total_roi,
-            read_roi=self.tasks[0].read_roi + (self.num_blocks,),
-            write_roi=self.tasks[0].write_roi + (self.num_blocks,),
-            status=daisy.BlockStatus.PROCESSING)
-        task_state = DummyTaskState(self.num_blocks, 1)
-
-        return block, task_state
-
-    def release_block(self, block):
-
-        return {self.tasks[0].task_id: DummyTaskState(self.num_blocks, 1)}
-
 class TestServer(unittest.TestCase):
 
     def test_basic(self):
@@ -55,14 +17,12 @@ class TestServer(unittest.TestCase):
                 check_function=None,
                 read_write_conflict=True,
                 fit='valid',
-                num_workers=2,
+                num_workers=1,
                 max_retries=2,
                 timeout=None)
 
-        scheduler = DummyScheduler([task], 3)
-
         server = daisy.Server()
-        server.run_blockwise([task], scheduler)
+        server.run_blockwise([task])
 
     def process_block(self, block):
         print("Processing block %s" % block)
