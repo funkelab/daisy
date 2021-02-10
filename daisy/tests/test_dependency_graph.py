082345ba2ce83e1ef4b3cb1f3f6813ba7884214a
pattonw <pattonw@hhmi.org>
2021-02-10 11:37:22 -0500

    update tests to be explicit about total_read_roi

--- daisy/tests/test_dependency_graph.py
+++ daisy/tests/test_dependency_graph.py
@@ -9,7 +9,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-total_roi = Roi((0,), (11,))
+total_roi = Roi((0,), (12,))
 read_roi = Roi((0,), (5,))
 write_roi_small = Roi((2,), (1,))
 write_roi_medium = Roi((1,), (3,))
@@ -47,25 +47,25 @@ tasks = [
 ]
 
 num_blocks = [
-    7,  # write roi should cover [2:9)
-    4,  # wrote roi should cover [1:9)
-    11, # write roi should cover [0:11)
-    5,  # write roi should cover [0:10)
-    7,  # write roi should cover [2:9) 
-    5,  # write roi should cover []
+    8,  # small_valid       write roi should cover [2:10)
+    3,  # medium_valid      write roi should cover [1:10)
+    8,  # small_overhang    write roi should cover [2:10)
+    4,  # medium_overhang   write roi should cover [1:12)
+    8,  # small_shrink      write roi should cover [2:10)
+    4,  # medium_shrink     write roi should cover [1:11)
 ]
 
 
-@pytest.mark.parametrize("task_blocks", list(zip(tasks, num_blocks))[:1])
+@pytest.mark.parametrize("task_blocks", list(zip(tasks, num_blocks)))
 def test_downstream(task_blocks):
     task, num_blocks = task_blocks
     graph = BlockwiseDependencyGraph(
         task.task_id,
-        task.total_roi,
         task.read_roi,
         task.write_roi,
         task.read_write_conflict,
         task.fit,
+        total_read_roi=total_roi,
     )
     num_enumerated = len(list(graph.enumerate_all_dependencies()))
     num_calculated = graph.num_blocks
