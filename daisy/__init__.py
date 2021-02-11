5e0a805761e1e61ace86672fd27f94b4d9bee5bb
Jan Funke <funkej@janelia.hhmi.org>
2021-02-11 15:43:28 -0500

    Add run_blockwise as convenience function

--- daisy/__init__.py
+++ daisy/__init__.py
@@ -1,10 +1,12 @@
 from __future__ import absolute_import
+from . import persistence # noqa
 from .array import Array # noqa
 from .block import Block, BlockStatus # noqa
 from .blocks import expand_roi_to_grid # noqa
 from .blocks import expand_write_roi_to_grid # noqa
 from .client import Client # noqa
 from .context import Context # noqa
+from .convenience import run_blockwise # noqa
 from .coordinate import Coordinate # noqa
 from .datasets import open_ds, prepare_ds # noqa
 from .dependency_graph import DependencyGraph, BlockwiseDependencyGraph # noqa
@@ -12,9 +14,7 @@ from .graph import Graph # noqa
 from .roi import Roi # noqa
 from .scheduler import Scheduler # noqa
 from .scheduler import distribute # noqa
-from .scheduler import run_blockwise # noqa
 from .server import Server # noqa
 from .task import Task # noqa
 from .worker import Worker # noqa
 from .worker_pool import WorkerPool # noqa
-from . import persistence # noqa
