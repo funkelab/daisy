e4728459b66ff28b39af589d7c80947e8048b37e
Jan Funke <funkej@janelia.hhmi.org>
2021-02-11 18:03:18 -0500

    Add CL monitor as ServerObserver

--- daisy/convenience.py
+++ daisy/convenience.py
@@ -1,6 +1,8 @@
 from .server import Server
+from .cl_monitor import CLMonitor
 
 
 def run_blockwise(tasks):
     server = Server()
+    cl_monitor = CLMonitor(server)
     server.run_blockwise(tasks)
