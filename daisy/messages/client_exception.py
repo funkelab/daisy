fd07b0854c899458f8c3c1cb9fc754d461c5b6a6
Jan Funke <funkej@janelia.hhmi.org>
2021-02-12 12:40:54 -0500

    Set full daisy context in ClientException

--- daisy/messages/client_exception.py
+++ daisy/messages/client_exception.py
@@ -2,6 +2,6 @@ from .message import ExceptionMessage
 
 
 class ClientException(ExceptionMessage):
-    def __init__(self, exception, client_id):
+    def __init__(self, exception, context):
         super().__init__(exception)
-        self.client_id = client_id
+        self.context = context
