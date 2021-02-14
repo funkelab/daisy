64f15f4ba41ccd5d042c15191b70033a9541baef
pattonw <pattonw@hhmi.org>
2021-02-13 21:40:27 -0500

    Scheduler: Update documentation and hide non-public functions

--- daisy/scheduler.py
+++ daisy/scheduler.py
@@ -19,15 +19,6 @@ class Scheduler:
 
     The Scheduler takes a list of tasks, and upon request will
     provide the next block available for processing.
-
-    Usage:
-
-    .. code:: python
-
-        graph = DependencyGraph(...)
-        return Scheduler().distribute(graph)
-
-    See the DependencyGraph class for more information.
     """
 
     def __init__(self, tasks: List[Task], count_all_orphans=False):
@@ -50,27 +41,10 @@ class Scheduler:
 
         self.count_all_orphans = count_all_orphans
 
-    def __init_task(self, task):
-        if task.task_id not in self.task_map:
-            self.task_map[task.task_id] = task
-            self.task_states[
-                task.task_id
-            ].total_block_count = self.dependency_graph.num_blocks(task.task_id)
-
-            for upstream_task in task.requires():
-                self.__init_task(upstream_task)
-
-    def has_next(self, task_id):
-        return self.task_queues[task_id].has_next()
-
-    def _queue_ready_block(self, block, index=None):
-        if index is None:
-            self.task_queues[block.task_id].ready_queue.append(block)
-        else:
-            self.task_queues[block.task_id].read_queue.insert(index, block)
-        self.task_states[block.task_id].ready_count += 1
-
     def get_ready_tasks(self) -> List[Task]:
+        """
+        Get a list of tasks that currently have blocks available for scheduling
+        """
         ready_tasks = []
         for task_id, task_state in self.task_states.items():
             if task_state.ready_count > 0:
@@ -92,14 +66,14 @@ class Scheduler:
             ``TaskState``:
                 The state of the task.
         """
-        if self.has_next(task_id):
-            block = self.task_queues[task_id].get_next()
+        block = self.task_queues[task_id].get_next()
+        if block is not None:
 
             # update states
             self.task_states[task_id].ready_count -= 1
             self.task_states[task_id].processing_count += 1
 
-            pre_check_ret = self.precheck(block)
+            pre_check_ret = self.__precheck(block)
             if pre_check_ret:
                 logger.debug(f"Skipping block ({block.block_id}); already processed.")
                 block.status = BlockStatus.SUCCESS
@@ -118,7 +92,7 @@ class Scheduler:
     def release_block(self, block):
         """
         Update the dependency graph with the status
-        of a given block ``block`` on task ``task``.
+        of a given block ``block``.
 
         Args:
             task(``Task``):
@@ -128,21 +102,20 @@ class Scheduler:
                 Block of interest.
 
         Return:
-            ``dict``(``task_id`` -> ``TaskStatus``):
+            ``dict``(``task_id`` -> ``TaskState``):
             Each task returned had its
             state changed by updating the status of the given
             block on the given task. i.e. if a task B was
             dependent on task A, and marking a block in A
             as solved made some blocks in B available for
-            processing, task B would update its state from
-            waiting to Waiting to Ready and be returned.
+            processing, task B would be returned with its state.
         """
         task_id = block.task_id
-        self.remove_from_processing_blocks(block)
+        self.__remove_from_processing_blocks(block)
         if block.status == BlockStatus.SUCCESS:
             new_blocks = self.ready_surface.mark_success(block)
             self.task_states[block.task_id].completed_count += 1
-            updated_tasks = self.update_ready_queue(new_blocks)
+            updated_tasks = self.__update_ready_queue(new_blocks)
             return updated_tasks
         if block.status == BlockStatus.FAILED:
             if (
@@ -156,24 +129,41 @@ class Scheduler:
                 self.task_states[block.task_id].orphaned_count += num_orphans
                 return {}
             else:
-                self._queue_ready_block(block)
+                self.__queue_ready_block(block)
                 self.task_queues[task_id].block_retries[block.block_id] += 1
                 return {task_id: self.task_states[task_id]}
         else:
             raise RuntimeError(f"Unexpected status for released block: {block.status}")
 
-    def remove_from_processing_blocks(self, block):
+    def __init_task(self, task):
+        if task.task_id not in self.task_map:
+            self.task_map[task.task_id] = task
+            self.task_states[
+                task.task_id
+            ].total_block_count = self.dependency_graph.num_blocks(task.task_id)
+
+            for upstream_task in task.requires():
+                self.__init_task(upstream_task)
+
+    def __queue_ready_block(self, block, index=None):
+        if index is None:
+            self.task_queues[block.task_id].ready_queue.append(block)
+        else:
+            self.task_queues[block.task_id].read_queue.insert(index, block)
+        self.task_states[block.task_id].ready_count += 1
+
+    def __remove_from_processing_blocks(self, block):
         self.task_queues[block.task_id].processing_blocks.remove(block.block_id)
         self.task_states[block.task_id].processing_count -= 1
 
-    def update_ready_queue(self, ready_blocks):
+    def __update_ready_queue(self, ready_blocks):
         updated_tasks = {}
         for ready_block in ready_blocks:
-            self._queue_ready_block(ready_block)
+            self.__queue_ready_block(ready_block)
             updated_tasks[ready_block.task_id] = self.task_states[ready_block.task_id]
         return updated_tasks
 
-    def precheck(self, block):
+    def __precheck(self, block):
         try:
             # pre_check can intermittently fail
             # so we wrap it in a try block
@@ -186,7 +176,3 @@ class Scheduler:
                 f"pre_check() exception for block {block.block_id}. " f"Exception: {e}",
             )
             return False
-
-
-def distribute():
-    raise NotImplementedError()
