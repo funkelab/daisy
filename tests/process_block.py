811f58a465250856a0aa2c09eafda3b612dbb90e
pattonw <pattonw@hhmi.org>
2021-12-20 17:06:31 -0500

    create file locks to allow testing to make sure subprocess closes

--- tests/process_block.py
+++ tests/process_block.py
@@ -2,13 +2,18 @@ import daisy
 
 import time
 import random
+import sys
 
+from filelock import FileLock
+
+tmp_path = sys.argv[1]
 
 client = daisy.Client()
 
-while True:
-    with client.acquire_block() as block:
-        if block is None:
-            break
-        else:
-            time.sleep(random.random())
+with FileLock(f"{tmp_path}/worker_{client.worker_id}.lock"):
+    while True:
+        with client.acquire_block() as block:
+            if block is None:
+                break
+            else:
+                time.sleep(random.random())
--- tests/test_clients_close.py
+++ tests/test_clients_close.py
@@ -1,20 +1,23 @@
 import daisy
+from daisy.logging import set_log_basedir
 
 import pytest
 
 import logging
 import subprocess
 import sys
+from filelock import FileLock
 
 logging.basicConfig(level=logging.DEBUG)
 
 
-def start_worker():
-    subprocess.run([sys.executable, "tests/process_block.py"])
+# @pytest.mark.skip()
+def test_workers_close(tmp_path):
+    set_log_basedir(tmp_path)
+    num_workers = 5
 
-
-@pytest.mark.skip()
-def test_workers_close():
+    def start_worker():
+        subprocess.run([sys.executable, "tests/process_block.py", f"{tmp_path}"])
 
     task = daisy.Task(
         "test_server_task",
@@ -25,10 +28,14 @@ def test_workers_close():
         check_function=None,
         read_write_conflict=True,
         fit="valid",
-        num_workers=5,
+        num_workers=num_workers,
         max_retries=2,
         timeout=None,
     )
 
     server = daisy.Server()
     server.run_blockwise([task])
+
+    for i in range(num_workers):
+        with FileLock(f"{tmp_path}/worker_{i}.lock", timeout=0.1):
+            pass
