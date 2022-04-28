b774dd91cf7cb778e7ee3609fefaa538a70aba08
Tri M. Nguyen <tri_nguyen@hms.harvard.edu>
2022-04-28 15:22:09 -0400

    Update requirements

--- setup.py
+++ setup.py
@@ -16,9 +16,13 @@ setup(
             'daisy.messages'
         ],
         install_requires=[
+            "networkx",
             "numpy",
             "tornado>=5",
             "networkx",
-            "pymongo"
+            "pymongo",
+            "tqdm",
+            "funlib.math @ git+https://github.com/funkelab/funlib.math@0c623f71c083d33184cac40ef7b1b995216be8ef",
+            "funlib.geometry @ git+https://github.com/funkelab/funlib.geometry@cf30e4d74eb860e46de40533c4f8278dc25147b1",
         ]
 )
