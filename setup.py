4569ef46136be95bfbd16883958e2b38430b30ab
pattonw <pattonw@hhmi.org>
2023-02-15 16:08:31 -0500

    update dependencies

--- setup.py
+++ setup.py
@@ -10,19 +10,13 @@ setup(
         license='MIT',
         packages=[
             'daisy',
-            'daisy.ext',
-            'daisy.persistence',
             'daisy.tcp',
             'daisy.messages'
         ],
         install_requires=[
-            "networkx",
-            "numpy",
             "tornado>=5",
-            "networkx",
-            "pymongo<4",
             "tqdm",
-            "funlib.math @ git+https://github.com/funkelab/funlib.math@0c623f71c083d33184cac40ef7b1b995216be8ef",
-            "funlib.geometry @ git+https://github.com/funkelab/funlib.geometry@cf30e4d74eb860e46de40533c4f8278dc25147b1",
+            "funlib.math",
+            "funlib.geometry",
         ]
 )
