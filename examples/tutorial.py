d1781d741f751c2b63992ff23ff807d96fe9fdca
William Patton <wllmpttn24@gmail.com>
2025-03-28 11:45:39 -0700

    move the quickstart examples before the in depth tutorial

--- examples/tutorial.py
+++ examples/tutorial.py
@@ -21,9 +21,261 @@ import multiprocessing
 
 multiprocessing.set_start_method("fork", force=True)
 
+
 # %% [markdown]
-# # Daisy Tutorial
-# Daisy is a library for processing large volumes in parallel. While other libraries (e.g. dask) can perform similar tasks, daisy is optimized for extremely large volumes, out-of-memory operations, and operations where neighboring blocks should not be run at the same time.
+# # Quick start
+#
+# This is a quick start tutorial with some basic examples.
+# If you would like a more detailed and in-depth tutorial, jump to: [In Depth Tutorial](#In-Depth-Tutorial)
+
+# %% [markdown]
+# ## Introduction and overview
+#
+# In this tutorial we will cover some basic Daisy `Task`s
+#
+# **daisy.Task**: All code you would like to distribute into blocks must be wrapped into a `Task` to be executed by daisy.
+#
+
+# %% [markdown]
+# ## Environment setup
+# If you have not already done so, I highly recommend you create an environment first.
+# You can do this with `uv` via:
+#
+# ```bash
+# uv init --python 3.12
+# ```
+# Activating the environment is as simple as `source .venv/bin/activate`
+#
+# Then, you can:
+# - add daisy directly to your dependencies with:
+#     ```bash
+#     uv add daisy
+#     ```
+# - install daisy using pip by name or via GitHub:
+#     ```bash
+#     pip install daisy
+#     ```
+#
+
+# %% [markdown]
+# ## Daisy Tasks
+
+# %% [markdown]
+# ### A Simple Task
+
+# %%
+import daisy  # noqa
+from daisy import Coordinate, Roi  # noqa
+import time  # noqa
+
+# Create a super simple task
+dummy_task = daisy.Task(
+    "Dummy",  # We give the task a name
+    total_roi=Roi((0,), (100,)),  # a 1-D bounding box [0,100)
+    read_roi=Roi((0,), (1,)),  # We read in blocks of size [0,1)
+    write_roi=Roi((0,), (1,)),  # We write in blocks of size [0, 1)
+    process_function=lambda block: time.sleep(
+        0.05
+    ),  # Our process function takes the block and simply waits
+    num_workers=5,
+)
+
+# execute the task without any multiprocessing
+daisy.run_blockwise([dummy_task], multiprocessing=False)
+daisy.run_blockwise([dummy_task], multiprocessing=True)
+
+# %% [markdown]
+# Since there are 100 blocks to process, and we always sleep for 0.05 seconds, it takes 5 seconds
+# to run all the blocks without multiprocessing, but only 1 second with `multiprocessing=True`.
+# When you run a task with `daisy`, you get a progress bar along with a summary
+# of the execution after you have finished processing. Here no errors were thrown so we completed all 100 blocks.
+#
+# `daisy` also programatically returns a dictionary containing a summary of the execution for all given
+# tasks so that you can decide how to procede based on whether any blocks failed.
+#
+# Next lets look see what we can do to modify this task
+
+# %% [markdown]
+# ### Failing blocks
+# %%
+
+import random  # noqa
+
+
+def random_fail(block: daisy.Block):
+    if random.random() < 0.2:
+        block.status = daisy.BlockStatus.FAILED
+    return block
+
+
+# Create a super simple task
+failing_task = daisy.Task(
+    "Failures",  # We give the task a name
+    total_roi=Roi((0,), (100,)),  # a 1-D bounding box [0,100)
+    read_roi=Roi((0,), (1,)),  # We read in blocks of size [0,1)
+    write_roi=Roi((0,), (1,)),  # We write in blocks of size [0, 1)
+    process_function=random_fail,  # Our process function takes the block and simply waits
+    max_retries=0,  # We do not retry failed blocks to get expected failure counts
+)
+
+# execute the task without any multiprocessing
+daisy.run_blockwise([failing_task], multiprocessing=False)
+
+# %% [markdown]
+
+# In this case, we have a 20% chance of failing each block. As you see from the execution summary,
+# about 20% of the blocks failed.
+# If your blocks are failing, you may want to rerun the task and only process failed blocks. This
+# is where we use the `check_function` argument in the `daisy.Task`.
+
+# %%
+import tempfile  # noqa
+from pathlib import Path  # noqa
+
+with tempfile.TemporaryDirectory() as tmpdir:
+
+    def random_fail(block: daisy.Block):
+        if random.random() < 0.2:
+            block.status = daisy.BlockStatus.FAILED
+        else:
+            Path(tmpdir, f"{block.block_id[1]}").touch()
+        return block
+
+    def check_block(block: daisy.Block) -> bool:
+        return Path(tmpdir, f"{block.block_id[1]}").exists()
+
+    while True:
+        # Create a super simple task
+        check_block_task = daisy.Task(
+            "Checking-Blocks",  # We give the task a name
+            total_roi=Roi((0,), (100,)),  # a 1-D bounding box [0,100)
+            read_roi=Roi((0,), (1,)),  # We read in blocks of size [0,1)
+            write_roi=Roi((0,), (1,)),  # We write in blocks of size [0, 1)
+            process_function=random_fail,  # Our process function takes the block and simply waits
+            check_function=check_block,  # Check if a block has been completed or not
+            max_retries=0,
+        )
+
+        # execute the task without any multiprocessing
+        task_state = daisy.run_blockwise([check_block_task], multiprocessing=False)
+        if task_state["Checking-Blocks"].failed_count == 0:
+            break
+
+# %% [markdown]
+
+# It took a few tries to complete the task since approximately 20% of the remaining blocks fail
+# on each attempt. We could also have set `max_retries` to a higher number to allow for retrying failed
+# blocks during the same task execution. This is to account for random failures due to network issues or
+# other random uncontrollable factors, but will not help if there is a logical bug in your code. In this
+# case since the failure is random, retrying does help.
+#
+# For more information see [Fault tolerance and the pre-check function](###Fault-tolerance-and-the-pre-check-function)
+
+# %% [markdown]
+# ### Different read/write sizes and boundary handling
+
+# %%
+
+# fit: "valid", "overhang", "shrink"
+
+# Create a super simple task
+overhang_task = daisy.Task(
+    "overhang",  # We give the task a name
+    total_roi=Roi((0,), (100,)),  # a 1-D bounding box [0,100)
+    read_roi=Roi((0,), (9,)),  # We read in blocks of size [0,1)
+    write_roi=Roi((2,), (5,)),  # We write in blocks of size [0, 1)
+    process_function=lambda b: ...,  # Empty process function
+    fit="overhang",
+)
+valid_task = daisy.Task(
+    "valid",  # We give the task a name
+    total_roi=Roi((0,), (100,)),  # a 1-D bounding box [0,100)
+    read_roi=Roi((0,), (9,)),  # We read in blocks of size [0,1)
+    write_roi=Roi((2,), (5,)),  # We write in blocks of size [0, 1)
+    process_function=lambda b: ...,  # Empty process function
+    fit="valid",
+)
+
+# %% [markdown]
+# because we now have:
+#
+# total roi: [0, 100)
+#
+# read roi: [0, 9)
+#
+# write roi: [2, 7)
+#
+# There is a "context" of 2 pixels between the read_roi and write roi.
+# Removing this context from the total roi gives us a "total write roi" of [2, 98).
+# Note that the total number of pixels to tile is 96, which is not evenly divisible
+# by the chunk size of 5. So our final block will have write roi of [97, 102) and a
+# read roi of [95, 104). Depending on your task, reading, and especially writing outside
+# of the given total roi bounds can cause problems.
+# in these cases we can handle the final block in a few ways. "valid": ignore the
+# final block. "overhang": process the final block. "shrink": shrink the read/write
+# rois until the read roi is fully contained in the total roi. In this case the "shrink"
+# strategy would shrink the read roi to [95, 100) and the write roi to [97, 98).
+#
+# Now when we executing the overhang task it will have one more block processed than the
+# valid task
+
+# %%
+daisy.run_blockwise([overhang_task], multiprocessing=False)
+daisy.run_blockwise([valid_task], multiprocessing=False)
+
+# %% [markdown]
+# ### Task Chaining
+#
+# It is also possible to chain multiple tasks together in such a way that
+# they either run in parallel or in sequence. If running in parallel, we
+# simply start the workers for both tasks at once. If running in sequence,
+# blocks will only be realeased once their dependencies in previous tasks
+# have been completed.
+
+# %%
+total_roi = Roi((0, 0), (1250, 1250))
+read_roi_shape = (400, 400)
+write_roi_shape = (250, 250)
+context = Coordinate(75, 75)
+
+task_a_to_b = daisy.Task(
+    "A_to_B",
+    total_roi=total_roi.grow(
+        context,
+        context,
+    ),
+    read_roi=Roi((0, 0), read_roi_shape),
+    write_roi=Roi(context, write_roi_shape),
+    process_function=lambda b: ...,
+    read_write_conflict=False,
+)
+task_b_to_c = daisy.Task(
+    "B_to_C",
+    total_roi=total_roi.grow(
+        context,
+        context,
+    ),
+    read_roi=Roi((0, 0), read_roi_shape),
+    write_roi=Roi(context, write_roi_shape),
+    process_function=lambda b: ...,
+    read_write_conflict=False,
+    upstream_tasks=[task_a_to_b],
+)
+
+daisy.run_blockwise([task_a_to_b, task_b_to_c], multiprocessing=False)
+
+# %% [markdown]
+#
+# Here is an mp4 visualization of the above task
+#
+# ![task chaining](_static/task_chaining.gif)
+
+
+# %% [markdown]
+# # In-Depth Tutorial
+# Daisy is a library for processing large volumes in parallel. While other libraries (e.g. dask)
+# can perform similar tasks, daisy is optimized for extremely large volumes, out-of-memory operations,
+# and operations where neighboring blocks should not be run at the same time.
 #
 # In this tutorial, we will cover:
 # - daisy terminology and concepts
@@ -1054,249 +1306,6 @@ axes[1].imshow(
 
 # %% [markdown]
 # **IMPORTANT PERFORMANCE NOTE:** Be aware that `read_write_conflicts` is set to `True` by default and can lead to performance hits in cases where you don't need it, so be sure to turn it off if you want every block to be run in parallel!
-
-# %% [markdown]
-# # Some Minimal Examples
-
-# %% [markdown]
-# ## Introduction and overview
-#
-# In this tutorial we will cover some basic Daisy `Task`s
-#
-# **daisy.Task**: All code you would like to distribute into blocks must be wrapped into a `Task` to be executed by daisy.
-#
-
-# %% [markdown]
-# ## Environment setup
-# If you have not already done so, I highly recommend you create an environment first.
-# You can do this with `uv` via:
-#
-# ```bash
-# uv init --python 3.12
-# ```
-# Activating the environment is as simple as `source .venv/bin/activate`
-#
-# Then, you can:
-# - add daisy directly to your dependencies with:
-#     ```bash
-#     uv add daisy
-#     ```
-# - install daisy using pip by name or via GitHub:
-#     ```bash
-#     pip install daisy
-#     ```
-#
-
-# %% [markdown]
-# ## Daisy Tasks
-
-# %% [markdown]
-# ### A Simple Task
-
-# %%
-import daisy  # noqa
-from daisy import Coordinate, Roi  # noqa
-import time  # noqa
-
-# Create a super simple task
-dummy_task = daisy.Task(
-    "Dummy",  # We give the task a name
-    total_roi=Roi((0,), (100,)),  # a 1-D bounding box [0,100)
-    read_roi=Roi((0,), (1,)),  # We read in blocks of size [0,1)
-    write_roi=Roi((0,), (1,)),  # We write in blocks of size [0, 1)
-    process_function=lambda block: time.sleep(
-        0.05
-    ),  # Our process function takes the block and simply waits
-    num_workers=5,
-)
-
-# execute the task without any multiprocessing
-daisy.run_blockwise([dummy_task], multiprocessing=False)
-daisy.run_blockwise([dummy_task], multiprocessing=True)
-
-# %% [markdown]
-# Since there are 100 blocks to process, and we always sleep for 0.05 seconds, it takes 5 seconds
-# to run all the blocks without multiprocessing, but only 1 second with `multiprocessing=True`.
-# When you run a task with `daisy`, you get a progress bar along with a summary
-# of the execution after you have finished processing. Here no errors were thrown so we completed all 100 blocks.
-#
-# `daisy` also programatically returns a dictionary containing a summary of the execution for all given
-# tasks so that you can decide how to procede based on whether any blocks failed.
-#
-# Next lets look see what we can do to modify this task
-
-# %% [markdown]
-# ### Failing blocks
-# %%
-
-import random  # noqa
-
-
-def random_fail(block: daisy.Block):
-    if random.random() < 0.2:
-        block.status = daisy.BlockStatus.FAILED
-    return block
-
-
-# Create a super simple task
-failing_task = daisy.Task(
-    "Failures",  # We give the task a name
-    total_roi=Roi((0,), (100,)),  # a 1-D bounding box [0,100)
-    read_roi=Roi((0,), (1,)),  # We read in blocks of size [0,1)
-    write_roi=Roi((0,), (1,)),  # We write in blocks of size [0, 1)
-    process_function=random_fail,  # Our process function takes the block and simply waits
-    max_retries=0,  # We do not retry failed blocks to get expected failure counts
-)
-
-# execute the task without any multiprocessing
-daisy.run_blockwise([failing_task], multiprocessing=False)
-
-# %% [markdown]
-
-# In this case, we have a 20% chance of failing each block. As you see from the execution summary,
-# about 20% of the blocks failed.
-# If your blocks are failing, you may want to rerun the task and only process failed blocks. This
-# is where we use the `check_function` argument in the `daisy.Task`.
-
-# %%
-import tempfile  # noqa
-from pathlib import Path  # noqa
-
-with tempfile.TemporaryDirectory() as tmpdir:
-
-    def random_fail(block: daisy.Block):
-        if random.random() < 0.2:
-            block.status = daisy.BlockStatus.FAILED
-        else:
-            Path(tmpdir, f"{block.block_id[1]}").touch()
-        return block
-
-    def check_block(block: daisy.Block) -> bool:
-        return Path(tmpdir, f"{block.block_id[1]}").exists()
-
-    while True:
-        # Create a super simple task
-        check_block_task = daisy.Task(
-            "Checking-Blocks",  # We give the task a name
-            total_roi=Roi((0,), (100,)),  # a 1-D bounding box [0,100)
-            read_roi=Roi((0,), (1,)),  # We read in blocks of size [0,1)
-            write_roi=Roi((0,), (1,)),  # We write in blocks of size [0, 1)
-            process_function=random_fail,  # Our process function takes the block and simply waits
-            check_function=check_block,  # Check if a block has been completed or not
-            max_retries=0,
-        )
-
-        # execute the task without any multiprocessing
-        task_state = daisy.run_blockwise([check_block_task], multiprocessing=False)
-        if task_state["Checking-Blocks"].failed_count == 0:
-            break
-
-# %% [markdown]
-
-# It took a few tries to complete the task since approximately 20% of the remaining blocks fail
-# on each attempt. We could also have set `max_retries` to a higher number to allow for retrying failed
-# blocks during the same task execution. This is to account for random failures due to network issues or
-# other random uncontrollable factors, but will not help if there is a logical bug in your code. In this
-# case since the failure is random, retrying does help.
-
-# %% [markdown]
-# ### Different read/write sizes and boundary handling
-
-# %%
-
-# fit: "valid", "overhang", "shrink"
-
-# Create a super simple task
-overhang_task = daisy.Task(
-    "overhang",  # We give the task a name
-    total_roi=Roi((0,), (100,)),  # a 1-D bounding box [0,100)
-    read_roi=Roi((0,), (9,)),  # We read in blocks of size [0,1)
-    write_roi=Roi((2,), (5,)),  # We write in blocks of size [0, 1)
-    process_function=lambda b: ...,  # Empty process function
-    fit="overhang",
-)
-valid_task = daisy.Task(
-    "valid",  # We give the task a name
-    total_roi=Roi((0,), (100,)),  # a 1-D bounding box [0,100)
-    read_roi=Roi((0,), (9,)),  # We read in blocks of size [0,1)
-    write_roi=Roi((2,), (5,)),  # We write in blocks of size [0, 1)
-    process_function=lambda b: ...,  # Empty process function
-    fit="valid",
-)
-
-# %% [markdown]
-# because we now have:
-#
-# total roi: [0, 100)
-#
-# read roi: [0, 9)
-#
-# write roi: [2, 7)
-#
-# There is a "context" of 2 pixels between the read_roi and write roi.
-# Removing this context from the total roi gives us a "total write roi" of [2, 98).
-# Note that the total number of pixels to tile is 96, which is not evenly divisible
-# by the chunk size of 5. So our final block will have write roi of [97, 102) and a
-# read roi of [95, 104). Depending on your task, reading, and especially writing outside
-# of the given total roi bounds can cause problems.
-# in these cases we can handle the final block in a few ways. "valid": ignore the
-# final block. "overhang": process the final block. "shrink": shrink the read/write
-# rois until the read roi is fully contained in the total roi. In this case the "shrink"
-# strategy would shrink the read roi to [95, 100) and the write roi to [97, 98).
-#
-# Now when we executing the overhang task it will have one more block processed than the
-# valid task
-
-# %%
-daisy.run_blockwise([overhang_task], multiprocessing=False)
-daisy.run_blockwise([valid_task], multiprocessing=False)
-
-# %% [markdown]
-# ### Task Chaining
-#
-# It is also possible to chain multiple tasks together in such a way that
-# they either run in parallel or in sequence. If running in parallel, we
-# simply start the workers for both tasks at once. If running in sequence,
-# blocks will only be realeased once their dependencies in previous tasks
-# have been completed.
-
-# %%
-total_roi = Roi((0, 0), (1250, 1250))
-read_roi_shape = (400, 400)
-write_roi_shape = (250, 250)
-context = Coordinate(75, 75)
-
-task_a_to_b = daisy.Task(
-    "A_to_B",
-    total_roi=total_roi.grow(
-        context,
-        context,
-    ),
-    read_roi=Roi((0, 0), read_roi_shape),
-    write_roi=Roi(context, write_roi_shape),
-    process_function=lambda b: ...,
-    read_write_conflict=False,
-)
-task_b_to_c = daisy.Task(
-    "B_to_C",
-    total_roi=total_roi.grow(
-        context,
-        context,
-    ),
-    read_roi=Roi((0, 0), read_roi_shape),
-    write_roi=Roi(context, write_roi_shape),
-    process_function=lambda b: ...,
-    read_write_conflict=False,
-    upstream_tasks=[task_a_to_b],
-)
-
-daisy.run_blockwise([task_a_to_b, task_b_to_c], multiprocessing=False)
-
-# %% [markdown]
-#
-# Here is an mp4 visualization of the above task
-#
-# ![task chaining](_static/task_chaining.gif)
 
 # %% [markdown]
 # # Map Reduce and Some simple benchmarks
